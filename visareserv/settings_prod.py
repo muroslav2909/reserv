f62fc416f021fc251f4ba1388bbd5a01432c9ef6
muroslav2909 <myroslav@preply.com>
2016-03-20 23:19:29 +0200

    deb

--- visareserv/settings_prod.py
+++ visareserv/settings_prod.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', '54.201.234.40']
 # STATIC_URL = '/static/'
 DOMAIN_NAME = '54.201.234.40'
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
