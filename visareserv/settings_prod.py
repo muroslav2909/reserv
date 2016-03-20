f4a9f1647bb3b52d08653772599d253cdfd662c6
muroslav2909 <myroslav@preply.com>
2016-03-20 17:14:28 +0200

    fff

--- visareserv/settings_prod.py
+++ visareserv/settings_prod.py
@@ -1,9 +1,5 @@
 import os
 
 DOMAIN_NAME = 'http://54.201.234.40'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 DEBUG = False
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
