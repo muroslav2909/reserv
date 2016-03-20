7580eeeb18a880b9365be2e8191cd2087678efc8
muroslav2909 <myroslav@preply.com>
2016-03-21 01:33:58 +0200

    ewfewfg

--- visareserv/settings_prod.py
+++ visareserv/settings_prod.py
@@ -3,6 +3,6 @@ import os
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', '54.201.234.40']
 # STATIC_URL = '/static/'
-DOMAIN_NAME = 'vamiko.if.ua'
+DOMAIN_NAME = 'http://vamiko.if.ua'
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
