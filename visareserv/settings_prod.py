69d21f0c5c712de8b41129ccd8b3bd2300084908
muroslav2909 <myroslav@preply.com>
2016-08-10 01:10:39 +0300

    efef

--- visareserv/settings_prod.py
+++ visareserv/settings_prod.py
@@ -2,7 +2,7 @@ import os
 from visareserv.settings import BASE_DIR
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', '54.201.234.40', 'vamiko.if.ua']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', '54.201.234.40', 'vamiko.if.ua', '54.149.186.44']
 
 # STATIC_URL = '/static/'
 DOMAIN_NAME = 'http://vamiko.if.ua/'
